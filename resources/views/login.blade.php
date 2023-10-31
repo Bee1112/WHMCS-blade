354991ecac171f50436ef6345cfa7f28b1bdee55
Bee1112 <124630874+Bee1112@users.noreply.github.com>
2023-11-01 00:36:11 +0800

    init commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en" data-theme="light">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="{{asset('assets/css/bootstrap.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/dark-theme.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/responsive.css')}}" rel="stylesheet">
+
+    <title>Login</title>
+  </head>
+  <body>
+
+  <section class="home-hero login">
+
+    <div class="badge reset-success">
+      <img src="{{asset('assets/img/reset-success.svg')}}" alt=""> Send reset instructions was succesful.
+    </div>
+
+    <div class="login-wrapper pb-0 w-100">
+      <div class="bg-dots bg-dots-left"></div>
+      <div class="bg-dots bg-dots-right"></div>
+
+      <div class="mt-5 text-center login-card-wrapper">
+
+          <div class="card-item login-card">
+              <div style="margin-top: -20px;" class="d-flex justify-content-center mb-3">
+                <img class="logo-dark" src="assets/img/crazy-rdp-logo.svg" alt="">
+                <img class="logo-light" src="assets/img/logo-light.svg" alt="">
+              </div>  
+
+
+              <h2 class="login-title">Login Account</h2>
+
+              <form action="route('login')" method="POST">
+                @csrf
+                <div class="login-input-wrapper mb-3">
+                  <label for="#email">Email Address</label>
+                  <input type="email" id="email" name="email" required placeholder="email@address.com">                
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="login-input-wrapper mb-4">
+                  <label for="#password">Password</label>
+                  <input type="password" id="password" name="password" required placeholder="••••••••••">      
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror          
+                </div>
+
+                <button class="btn-dark w-100 mb-2" type="submit">Login</button>
+
+                <button class="btn-light w-100 mb-3" onClick="{{URL::to('/login/signup')}}">Register account</button>
+
+                <div class="text-center">
+                    <a class="fs-14" href="#">Forgot password?</a>
+                </div>
+
+              </form>
+
+
+
+
+          </div>
+      </div>       
+    </div>
+  </section>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="{{asset('assets/js/scripts.js')}}"></script>
+  </body>
+</html>
